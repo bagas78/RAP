99c33f35bdcd7689c3de849ef91d422aed8cc7e0
Bagas Pramono <57339494+bagas78@users.noreply.github.com>
2023-07-01 00:11:24 +0700

    update

--- application/views/keuangan/coa.php
+++ application/views/keuangan/coa.php
@@ -18,32 +18,57 @@
           <table class="table table-bordered" style="width: 100%;">
             <thead>
               <tr>
-                <td>Nomor</td>
-                <td>Akun</td>
-                <td>Mutasi</td>
+                <td rowspan="2" width="1">Nomor</td>
+                <td rowspan="2">Normal Akun</td>
+                <td colspan="2" align="center">Mutasi</td>
               </tr>
-              <tr colspan="2">
-                <td>+</td>
-                <td>-</td>
+              <tr>
+                <td style="background: lightgreen;"><i class="fa fa-plus"></i></td>
+                <td style="background: pink;"><i class="fa fa-minus"></i></td>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>111</td>
+                <td>Modal</td>
+                <td>Kredit</td>
+                <td>Debit</td>
+              </tr>
+              <tr>
+                <td>121</td>
+                <td>Harta</td>
+                <td>Debit</td>
+                <td>Kredit</td>
+              </tr>
+              <tr>
+                <td>131</td>
+                <td>Utang</td>
+                <td>Kredit</td>
+                <td>Debit</td>
+              </tr>
+              <tr>
+                <td>141</td>
+                <td>Pendapatan</td>
+                <td>Kredit</td>
+                <td>Debit</td>
+              </tr>
+              <tr>
+                <td>151</td>
+                <td>Beban</td>
+                <td>Debit</td>
+                <td>Kredit</td>
               </tr>
             </tbody>
           </table>
           
-          <table id="example" class="table table-bordered table-hover" style="width: 100%;">
+          <div class="clearfix"></div><br/>
+
+          <table class="table table-bordered table-hover" style="width: 100%;">
                 <thead>
                 <tr>
                   <th width="1">Nomor</th>
                   <th>Akun</th>
                   <th>Normal Akun</th>
-                  <th>Plus (+)</th>
-                  <th>Min (-)</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -51,9 +76,7 @@
                     <tr>
                       <td><?=@$val['coa_nomor']?></td>
                       <td><?=@$val['coa_akun']?></td>
-                      <td><?=@$val['coa_sub_akun'].' ( '.@$val['coa_sub_nomor'].' ) '?></td>
-                      <td><?= (@$val['coa_sub_plus'] == 'K')?'<span style="background: papayawhip; padding: 2%;" span>Kredit</span>':'<span style="background: lightgreen; padding: 2%;">Debit</span>'?></td>
-                      <td><?= (@$val['coa_sub_min'] == 'K')?'<span style="background: papayawhip; padding: 2%;" span>Kredit</span>':'<span style="background: lightgreen; padding: 2%;">Debit</span>'?></td>
+                      <td><?=@$val['coa_sub_akun']?></td>
                     </tr>
                   <?php endforeach ?>
                 </tbody>
